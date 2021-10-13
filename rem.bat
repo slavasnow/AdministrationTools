02beb421b4d510af9031ad5468b5db663c2b01bc
slavasnow <51907565+slavasnow@users.noreply.github.com>
2021-10-13 09:28:01 +0300

    new version 0.2
    Added hiding password entry

--- rem.bat
+++ rem.bat
@@ -1,10 +1,29 @@
 @echo off
-@echo rename PC
+echo.
+echo.
 
-set /P oldNamePC="OLd Name PC: "
-set /P newNamePC="New Name PC: "
-set /P PWD="Your password: "
+echo ########  ######## ##    ##    ###    ##     ## ########    ########   ######
+echo ##     ## ##       ###   ##   ## ##   ###   ### ##          ##     ## ##    ##
+echo ##     ## ##       ####  ##  ##   ##  #### #### ##          ##     ## ##
+echo ########  ######   ## ## ## ##     ## ## ### ## ######      ########  ##
+echo ##   ##   ##       ##  #### ######### ##     ## ##          ##        ##
+echo ##    ##  ##       ##   ### ##     ## ##     ## ##          ##        ##    ##
+echo ##     ## ######## ##    ## ##     ## ##     ## ########    ##         ######    version 0.2
 
+
+
+echo.
+echo.
+
+set /P oldNamePC="Old PC name : "
+set /P newNamePC="New PC name : "
+
+rem Запрос на ввод пароля, скрывает символы
+set "psCommand=powershell -Command "$pword = read-host 'Your Password' -AsSecureString ; ^
+      $BSTR=[System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($pword); ^
+            [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)""
+for /f "usebackq delims=" %%p in (`%psCommand%`) do set PWD=%%p
+::echo %PWD% Проверка вывода пароля
 
 netdom RENAMECOMPUTER %oldNamePC%  /newname: %newNamePC% /userd: %USERNAME% /passwordd: %PWD%
 
